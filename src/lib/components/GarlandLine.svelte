7ef38c1bd052815b10049b361240548f8a8e18ab
Milan <milan@MacBook-Air-von-Milan.local>
2026-02-06 20:07:42 +0100

    increase garland line width to 12px

--- src/lib/components/GarlandLine.svelte
+++ src/lib/components/GarlandLine.svelte
@@ -118,7 +118,7 @@
 		d={pathD}
 		fill="none"
 		stroke="var(--color-line)"
-		stroke-width="6"
+		stroke-width="12"
 		stroke-dasharray={totalLength}
 		stroke-dashoffset={dashOffset}
 		style="will-change: stroke-dashoffset;"
